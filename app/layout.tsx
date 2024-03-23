f0d096f06fdc577e0152e8d5a88b41bd7ff1664a
tyecode <sengphachanh.dev@gmail.com>
2024-03-23 15:21:00 +0700

    chore: Install vercel speed insignts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import './globals.css'
 
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body suppressHydrationWarning={true}>
+        <SpeedInsights />
         <ThemeProvider
           attribute='class'
           defaultTheme='light'
