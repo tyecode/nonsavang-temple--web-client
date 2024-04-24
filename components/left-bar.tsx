eaf260e4a60f23763dab8d97a4c00c0df8ff2671
tyecode <sengphachanh.dev@gmail.com>
2024-04-24 21:08:01 +0700

    fix: Import 'next/navigation' issue

--- components/left-bar.tsx
+++ components/left-bar.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react'
 
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 import { Nav } from '@/components/nav'
 import { Separator } from '@/components/ui/separator'
